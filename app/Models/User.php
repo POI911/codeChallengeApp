91de23e0b6ecaa7db193ff9e4063539a349acb06
POI911 <abodoom539@gmail.com>
2022-11-19 01:43:31 +0300

    auto encrpy password

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -29,7 +31,9 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    
+    protected $guarded = [];
+
+
 
     /**
      * The attributes that should be cast.
@@ -39,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+
 }
