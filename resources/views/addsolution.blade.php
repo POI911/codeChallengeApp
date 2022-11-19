2652803e06a1f60242b7f6bfcec6f037b3295944
POI911 <abodoom539@gmail.com>
2022-11-19 21:59:45 +0300

    just renamed the file from submitsoulution to addsoultion

---
+++ resources/views/addsolution.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles1.css" />
+
+    <title>To-Do Challenges</title>
+  </head>
+  <body>
+      <h1 class="md:text-5xl text-4xl text-raizer-400 text-center pt-12 md:pt-32 mb-12">
+          Submit Solutions
+        </h1>
+
+        <!-- <div class="container mx-auto"> -->
+            @auth
+    <form class=" mx-auto mb-7 md:w-1/2 px-8 " method="POST" action="/addsolution">
+      <div class="flex justify-evenly flex-col gap-3 mb-5">
+        @csrf
+
+
+      </div>
+      <div class="flex justify-evenly flex-col gap-3 mb-5">
+        <label for="challengeID" class="text-xl mr-24 "
+          >Challenge Name
+        </label>
+        <input
+          class=" border-solid border-2 rounded-lg text-lg px-3 py-2 border-raizer-400"
+          type="text"
+
+          name="challengeName"
+        />
+      </div>
+      <div class="flex justify-evenly flex-col gap-3 mb-5">
+        <label for="solutionID" class="text-xl mr-24 "
+          >Solution Link
+        </label>
+        <input
+          class=" border-solid border-2 rounded-lg text-lg px-3 py-2 border-raizer-400"
+          type="text"
+          name="solutionLink"
+        />
+      </div>
+      <div class="flex justify-evenly flex-col">
+        <button class="text-2xl" type="submit">Submit</button>
+      </div>
+    </form>
+    @else
+    <div class=" mx-auto mb-7 md:w-1/2 px-8 ">
+    <p class="text-xl mr-24" >You have to <a href='/login'>log in</a> First</p>
+    </div>
+    @endauth
+ <!-- -->
+    <div class=" mx-auto mb-7 md:w-1/2 px-8 ">
+        <button class="mt-8">
+          <a class="text-raizer-400 py-5 px-6 bg-slate-800 rounded-full" href="/"
+            ><i class="fa-solid fa-arrow-left text-lg md:text-xl"></i
+          ></a>
+        </button>
+      </div>
+
+  </body>
+  <script
+    src="https://kit.fontawesome.com/33ef633056.js"
+    crossorigin="anonymous"
+  ></script>
+</html>
